4efd9f494e1d38aaec46bbecb4774fe38a3053c3
JobsJr <r.rajeev434@gmail.com>
2016-12-11 14:19:52 +0530

    Find magic number

---
+++ src/com/example/bitmanipulation/MagicNumber.java
@@ -0,0 +1,29 @@
+package com.example.bitmanipulation;
+
+public class MagicNumber {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=3;i++){
+			System.out.println(i+" th magic number is "+getMagicNumber(i));
+		}
+
+	}
+	
+	
+	private static int getMagicNumber(int n){
+		int magicNumber=0;
+		int pow=1;
+		while(n!=0){
+			pow=pow*5;
+			if((n&1)!=0){
+				magicNumber+=pow;
+			}
+			n>>=1;
+		}
+		
+		return magicNumber;
+		
+	}
+
+
+}
